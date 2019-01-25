678430d7860fb17e85174f0963f1626feab4fa05
wcolts2000 <wcolts2000@gmail.com>
2019-01-25 09:08:38 -0800

    register and protected jokes endpoints functioning, need to fix login logic still

--- README.md
+++ README.md
@@ -59,15 +59,15 @@ Follow these steps to set up and work on your project:
 
 Follow these steps for completing your project:
 
-- [ ] `cd` into the root of the project and run `yarn` to install dependencies.
-- [ ] Once you have your `node_modules` go ahead and run `yarn server` or `npm run server` to start your node server.
-- [ ] Submit a Pull-Request to merge <firstName-lastName> Branch into master (student's Repo).
-- [ ] Add your Project Manager as a Reviewer on the Pull-request
-- [ ] PM then will count the HW as done by merging the branch back into master.
+- [ x ] `cd` into the root of the project and run `yarn` to install dependencies.
+- [ x ] Once you have your `node_modules` go ahead and run `yarn server` or `npm run server` to start your node server.
+- [ x ] Submit a Pull-Request to merge <firstName-lastName> Branch into master (student's Repo).
+- [ x ] Add your Project Manager as a Reviewer on the Pull-request
+- [ x ] PM then will count the HW as done by merging the branch back into master.
 
 Helpful Tip on Testing this Project:
 
-- [ ] **TEST** this project using **`POSTMAN`**.
+- [ x ] **TEST** this project using **`POSTMAN`**.
 
 ## Minimum Viable Product
 
--- config/routes.js
+++ config/routes.js
@@ -1,32 +1,71 @@
-const axios = require('axios');
+const express = require("express");
+const bcrypt = require("bcryptjs");
+const axios = require("axios");
+const db = require("../database/dbConfig");
 
-const { authenticate } = require('../auth/authenticate');
+const { authenticate, generateToken } = require("../auth/authenticate");
 
 module.exports = server => {
-  server.post('/api/register', register);
-  server.post('/api/login', login);
-  server.get('/api/jokes', authenticate, getJokes);
+  server.post("/api/register", register);
+  server.post("/api/login", login);
+  server.get("/api/jokes", authenticate, getJokes);
 };
 
-function register(req, res) {
+// const router = express.Router();
+
+function register(req, res, next) {
   // implement user registration
+  // router.post("/", (req, res) => {});
+  const user = req.body;
+  user.password = bcrypt.hashSync(user.password, 14);
+  if (user.username && user.password) {
+    db("users")
+      .insert(user)
+      .then(userId => {
+        const token = generateToken(req.body);
+        res.status(201).json({ message: `welcome ${user.username}`, token });
+      })
+      .catch(err => {
+        if (err.errno === 19) {
+          res
+            .status(400)
+            .json({ message: "username taken, please try another" });
+        } else {
+          next(err);
+        }
+      });
+  }
 }
 
 function login(req, res) {
   // implement user login
+  const userCreds = req.body;
+  db("users")
+    .where({ username: userCreds.username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(userCreds, user.password)) {
+        const token = generateToken(user);
+        res.json({ message: `welcome ${user.username}`, token });
+      } else {
+        res.status(401).json({ message: "You shall not pass!" });
+      }
+    })
+    .catch(err => res.status(500).json({ message: "You shall not pass!" }));
 }
 
 function getJokes(req, res) {
+  const token = req.headers.authorization;
   const requestOptions = {
-    headers: { accept: 'application/json' },
+    headers: { accept: "application/json", authorization: token }
   };
 
   axios
-    .get('https://icanhazdadjoke.com/search', requestOptions)
+    .get("https://icanhazdadjoke.com/search", requestOptions)
     .then(response => {
       res.status(200).json(response.data.results);
     })
     .catch(err => {
-      res.status(500).json({ message: 'Error Fetching Jokes', error: err });
+      res.status(500).json({ message: "Error Fetching Jokes", error: err });
     });
 }
